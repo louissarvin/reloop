396380aaf8a3aa0bc78d99b51c67e74ce4aae9d8
louissarvin <louisbaxel@gmail.com>
2026-01-15 19:35:50 +0700

    fix: cors and typescript error

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,7 @@ app.use("*", logger());
 app.use(
   "*",
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174", "https://reloop.vercel.app", "https://www.reloop.vercel.app"],
+    origin: ["http://localhost:5173", "http://localhost:5174", "https://reloop-tau.vercel.app", "https://www.reloop-tau.vercel.app"],
     allowMethods: ["GET", "POST", "OPTIONS"],
     allowHeaders: ["Content-Type"],
   })
